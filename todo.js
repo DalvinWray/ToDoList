6874d2d56e934e07a2e4b8ac8ebf8e6c2334728c
DalvinWray <122240870+DalvinWray@users.noreply.github.com>
2023-02-04 11:59:59 -0500

    todo list

---
+++ todo.js
@@ -0,0 +1,17 @@
+//Selectors
+
+const add= document.querySelector("#add")
+
+
+
+
+//Event Listners
+add.addEventListener('click',addToList)
+
+
+
+
+//Functions
+function addToList(event){
+    
+}
